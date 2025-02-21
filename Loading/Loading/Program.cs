4128105e193551f6e51a1dc1cac5cba1c802aca8
leecmin <light01234@naver.com>
2025-02-21 15:04:22 +0900

    loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear();
+            Console.WriteLine("텍스트엘든링");
+
+            Console.WriteLine("스토리1");
+            
+
+
+        }
+    }
+}
