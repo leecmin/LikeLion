d9249b0c2ee2883a9ec0b8fb013fe3af067e126d
leecmin <light01234@naver.com>
2025-02-21 10:22:08 +0900

    study2

---
+++ Study2/Study2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 25;
+
+            Console.WriteLine(age);
+        }
+    }
+}
