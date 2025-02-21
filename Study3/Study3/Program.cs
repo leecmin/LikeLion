48d5e83c9e6a5bfa2631accebea60f2c87e2bb3c
leecmin <light01234@naver.com>
2025-02-21 10:43:21 +0900

    study3

---
+++ Study3/Study3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            string 캐릭터이름 = "Alice";
+            char 등급 = 'S';
+
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(캐릭터이름);
+            Console.WriteLine(등급);
+        }
+    }
+}
