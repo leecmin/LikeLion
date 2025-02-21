d2067ab56fa0e05f5e12a9ecd30fa0d1959c0a26
leecmin <light01234@naver.com>
2025-02-21 14:35:09 +0900

    study4

---
+++ study4/study4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string greeting;
+            //greeting = "Hello World!";
+
+            //Console.WriteLine(greeting);
+
+            //int score = 100;
+            //double temperature = 36.5;
+            //string city = "Seoul";
+
+            //Console.WriteLine(score);
+            //Console.WriteLine(temperature);
+            //Console.WriteLine(city);
+
+            //int x = 10, y = 20, z = 30;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            //const double Pi = 3.14159;
+            //const int MaxScore = 100;
+
+            //Console.WriteLine("Pi :" + Pi);
+            //Console.WriteLine("MaxScore :" + MaxScore);
+
+            //int a = 16755, b = 78103, c = 36, d = 1017, e = 41, f = 611, g = 22, h = 39;
+
+            //Console.WriteLine("공격력 :" + a);
+            //Console.WriteLine("최대 생명력 :" + b);
+            //Console.WriteLine("치명 :" + c);
+            //Console.WriteLine("특화 :" + d);
+            //Console.WriteLine("제압 :" + e);
+            //Console.WriteLine("신속 :" + f);
+            //Console.WriteLine("인내 :" + g);
+            //Console.WriteLine("숙련 :" + h);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ study5/study5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int integerNum = 10;
+            //float floatNum = 3.14f;
+            //double doubleNum = 3.14159;
+
+
+            //Console.WriteLine(integerNum); 
+            //Console.WriteLine(floatNum); 
+            //Console.WriteLine(doubleNum);
+
+
+            //int intValue = -100;
+            //long longValue = 1234567890L;
+
+            //Console.WriteLine(intValue);
+            //Console.WriteLine(longValue);
+
+            //string greeting = "Hello World";
+            //string name = "Alice";
+
+            //Console.Write(greeting +" "+ name);
+
+            //bool isRunning = true;
+            //bool isFinished = false;
+
+            //Console.WriteLine(isRunning);
+            //Console.WriteLine(isFinished);
+
+            //const double Pi = 3.14159;
+            //const int Number = 100;
+
+            //Console.WriteLine(Pi);
+            //Console.WriteLine(Number);
+
+            //System.Int32 number = 123;
+            //System.String text = "Hello";
+            //System.Boolean flag = true;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(text);
+            //Console.WriteLine(flag);
+
+            int number = 123;
+
+            string numberAsString = number.ToString();
+
+            bool flag = true;
+
+            string flagAsString = flag.ToString();
+
+            Console.WriteLine(numberAsString);
+            Console.WriteLine(flagAsString);
+
+
+
+
+        }
+    }
+}
